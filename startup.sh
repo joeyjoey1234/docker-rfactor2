be9e96968099232b86669008df4b458db3c601f2
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2022-03-21 00:10:35 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,11 @@
 # restarts the xdm service
 cd /home/docker/.wine/drive_c/racing/STEAMCMD/
 wine /home/docker/.wine/drive_c/racing/STEAMCMD/steamcmd.exe +login anonymous +force_install_dir ../rFactor2-Dedicated +app_update 400300 +quit
+cp /home/docker/.wine/drive_c/racing/rfactor2-dedicated/Bin64/ModMgr.exe /home/docker/.wine/drive_c/racing/rfactor2-dedicated/MogMgr.exe
+cp /home/docker/.wine/drive_c/racing/rfactor2-dedicated/Bin64/rFactor2\ Dedicated.exe ../rFactor2\ Dedicated.exe
+cp /home/docker/.wine/drive_c/racing/rfactor2-dedicated/Support/Tools/MAS2_x64.exe /home/docker/.wine/drive_c/racing/rfactor2-dedicated/MAS2_x64.exe
+
+
 /etc/init.d/xdm restart
 
 # Start the ssh service
